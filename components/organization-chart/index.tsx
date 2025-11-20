705ea7bd7488129ddc855475c500b3e50a8ada9a
dinhdkhoa <kevin_mccain1718@yahoo.com>
2025-11-20 13:29:04 +0700

    feat: Org Chart Update

--- components/organization-chart/index.tsx
+++ components/organization-chart/index.tsx
@@ -26,57 +26,121 @@ interface Organization {
     departments: DepartmentData[]
 }
 
-
 const organizationData: Organization[] = [
+
     {
+
         manager: {
-            name: 'Ngo Anh Quan',
+
+            name: 'Ngo Anh QUAN',
+
             id: 'GS/OSD6-APAC23 | GS/PSD63',
+
             title: 'Logistics Planning, Operations, and Digitalization Manager',
+
             image: '/images/avatar-placeholder.png',
+
         },
+
         departments: [
+
             {
+
                 name: 'Planning',
+
                 teams: [
+
                     [
-                        { name: 'Nguyen Duong Hoai PHUONG', id: 'VM-JP', role: 'Source Planning', image: '/images/avatar-placeholder.png' },
-                        { name: 'Tran Minh Anh THU', id: 'EM-CA, IPO-JP', role: 'Passthrough Planning', image: 'https://res.cloudinary.com/dr9bxbmwi/image/upload/v1763557597/Phan_Thi_Na_rc5ine.png' },
-                        { name: 'Nguyen Tran Ngoc ANH', id: 'PS-JP', role: 'Source Planning', image: 'https://res.cloudinary.com/dr9bxbmwi/image/upload/v1763557598/Tran_Vu_Ngoc_Tran_eu0oph.png' },
-                        { name: 'Trinh Thi Thien KIM', id: 'MusP/LDP-JP', role: 'Source Planning', image: 'https://res.cloudinary.com/dr9bxbmwi/image/upload/v1763557598/Tran_Vu_Ngoc_Tran_eu0oph.png' },
+
+                        { name: 'Nguyen Duong Hoai PHUONG', id: 'VM-JP', role: 'Source Planning', image: 'https://res.cloudinary.com/dr9bxbmwi/image/upload/v1763557574/Nguyen_Duong_Hoai_Phuong_xqmyhq.jpg' },
+
+                        { name: 'Tran Minh Anh THU', id: 'EM-CA, IPO-JP', role: 'Passthrough Planning', image: 'https://res.cloudinary.com/dr9bxbmwi/image/upload/v1763557582/Tran_Minh_Anh_Thu_hccdm3.jpg' },
+
+                        { name: 'Nguyen Tran Ngoc ANH', id: 'PS-JP', role: 'Source Planning', image: 'https://res.cloudinary.com/dr9bxbmwi/image/upload/v1763557576/Nguyen_Tran_Ngoc_Anh_ojmgaz.png' },
+
+                        { name: 'Trinh Thi Thien KIM', id: 'MusP/LDP-JP', role: 'Source Planning', image: 'https://res.cloudinary.com/dr9bxbmwi/image/upload/v1763557586/Trinh_Thi_Thien_Kim_hhmseq.jpg' },
+
                     ],
+
                 ],
+
             },
+
             {
+
                 name: 'Operations',
+
                 teams: [
+
                     [
-                        { name: 'Ly My HAN', id: 'VM-JP', role: 'Logistics Operations', image: 'https://res.cloudinary.com/dr9bxbmwi/image/upload/v1763557586/Vuong_Kim_Lien_snzwc6.jpg' },
-                        { name: 'Tran Duc THAO', id: 'PS-JP & VM-JP', role: 'Logistics Operations', image: '/images/avatar-placeholder.png' },
-                        { name: 'Le Van KHOA', id: 'VM-JP', role: 'Logistics Operations', image: '/images/avatar-placeholder.png' },
-                        { name: 'Tran Thi THUY', id: 'VM-JP', role: 'Logistics Operations', image: '/images/avatar-placeholder.png' },
-                        { name: 'Nguyen Thi My DUYEN', id: 'GS/OSD1-JP', role: 'Logistics Operations', image: '/images/avatar-placeholder.png' },
+
+                        { name: 'Ly My HAN', id: 'GS/OSD6 APAC23 VM-JP', role: 'Logistics Operations', image: 'https://res.cloudinary.com/dr9bxbmwi/image/upload/v1763557573/Ly_My_Han_g4zzke.jpg' },
+
+                        { name: 'Tran Duc THAO', id: 'GS/OSD6 APAC23 PS-JP & VM-JP', role: 'Logistics Operations', image: 'https://res.cloudinary.com/dr9bxbmwi/image/upload/v1763557582/Tran_Duc_Thao_awcl4v.jpg' },
+
+                        { name: 'Ta Quang TRI', id: 'GS/OSD6 APAC23 PS-JP, DCEA', role: 'Logistics Operations', image: 'https://res.cloudinary.com/dr9bxbmwi/image/upload/v1763557579/Ta_Quang_Tri_nf8mo2.jpg' },
+
+                        { name: 'Le Van KHOA', id: 'GS/OSD6 APAC23 VM-JP', role: 'Logistics Operations', image: 'https://res.cloudinary.com/dr9bxbmwi/image/upload/v1763557572/Le_Van_Khoa_p3knbv.jpg' },
+
+                        { name: 'Tran Thi THUY', id: 'GS/OSD6 APAC23 VM-JP', role: 'Logistics Operations', image: 'https://res.cloudinary.com/dr9bxbmwi/image/upload/v1763557584/Tran_Thi_Thuy_poldvo.jpg' },
+
+                        { name: 'Nguyen Thi My DUYEN', id: 'GS/OSD6 APAC23 GS/OSD1-JP', role: 'Logistics Operations', image: 'https://res.cloudinary.com/dr9bxbmwi/image/upload/v1763557574/Nguyen_Thi_My_Duyen_wkc1pq.jpg' },
+
+                        { name: 'Vuong Kim LIEN', id: 'GS/OSD6 APAC23 GS/OSD1-JP', role: 'Logistics Operations', image: 'https://res.cloudinary.com/dr9bxbmwi/image/upload/v1763612280/Vuong_Kim_Lien_snzwc6.jpg' },
+
                     ],
+
                     [
-                        { name: 'Lam Duc KHOA', id: 'VM-JP', role: 'Logistics Operations', image: '/images/avatar-placeholder.png' },
-                        { name: 'Bui Tu Anh THU', id: 'PS-JP', role: 'Logistics Operations', image: '/images/avatar-placeholder.png' },
-                        { name: 'Nguyen Van TRONG', id: 'ME-JP', role: 'Logistics Operations', image: '/images/avatar-placeholder.png' },
-                        { name: 'Nguyen Thuy VY', id: 'IPO-JP', role: 'Logistics Operations', image: '/images/avatar-placeholder.png' },
+
+                        { name: 'Lam Duc KHOA', id: 'GS/OSD6 APAC23 VM-JP', role: 'Logistics Operations', image: 'https://res.cloudinary.com/dr9bxbmwi/image/upload/v1763557573/Lam_Duc_Khoa_ww5qoz.jpg' },
+
+                        { name: 'Bui Tu Anh THU', id: 'GS/OSD6 APAC23 PS-JP', role: 'Logistics Operations', image: 'https://res.cloudinary.com/dr9bxbmwi/image/upload/v1763619049/Bui_Tu_Anh_Thu_z0qy7h.jpg' },
+
+                        { name: 'Nguyen Van TRONG', id: 'GS/OSD6 APAC23 ME-JP', role: 'Logistics Operations', image: 'https://res.cloudinary.com/dr9bxbmwi/image/upload/v1763557576/Nguyen_Van_Trong_ycco2d.jpg' },
+
+                        { name: 'Nguyen Y BAO', id: 'GS/OSD6 APAC23 ME-JP', role: 'Logistics Operations', image: 'https://res.cloudinary.com/dr9bxbmwi/image/upload/v1763612825/Nguyen_Y_Bao_cwgjqf.png' },
+
+                        { name: 'Nguyen Thuy VY', id: 'GS/OSD6 APAC23 IPO-JP', role: 'Logistics Operations', image: 'https://res.cloudinary.com/dr9bxbmwi/image/upload/v1763609408/Nguyen_Thuy_Vy_calqfh.png' },
+
+                        { name: 'Phan Nguyen NGOC', id: 'GS/OSD6 APAC23 GS/OSD1-JP', role: 'Logistics Operations', image: 'https://res.cloudinary.com/dr9bxbmwi/image/upload/v1763557578/Phan_Nguyen_Ngoc_yaeeff.jpg' },
+
+                        { name: 'Hoang Khanh LINH', id: 'GS/OSD6 APAC23 PS/LOP5-JP', role: 'Logistics Intern', image: 'https://res.cloudinary.com/dr9bxbmwi/image/upload/v1763618990/Hoang_Khanh_Linh_itkx7w.jpg' },
+
+
+
                     ],
+
                 ],
+
             },
+
             {
+
                 name: 'Digitalization',
+
                 teams: [
+
                     [
-                        { name: 'Tran Ngoc DAN', id: 'GS/OSD6-APAC2', role: 'DA & Digitalization', image: '/images/avatar-placeholder.png' },
-                        { name: 'Phan Thi Ha GIANG', id: 'GS/PSD63', role: 'DA & Digitalization', image: '/images/avatar-placeholder.png' },
-                        { name: 'Dinh Dang KHOA', id: 'GS/PSD63', role: 'DA & Digitalization', image: '/images/avatar-placeholder.png' },
+
+                        { name: 'Tran Ngoc DAN', id: 'GS/OSD6-APAC23', role: 'DA & Digitalization', image: 'https://res.cloudinary.com/dr9bxbmwi/image/upload/v1763557583/Tran_Ngoc_Dan_wgbrqn.jpg' },
+
+                        { name: 'Phan Thi Ha GIANG', id: 'GS/PSD63', role: 'DA & Digitalization', image: 'https://res.cloudinary.com/dr9bxbmwi/image/upload/v1763557578/Phan_Thi_Ha_Giang_hfywsc.jpg' },
+
+                        { name: 'Dinh Dang KHOA', id: 'GS/PSD63', role: 'DA & Digitalization', image: 'https://res.cloudinary.com/dr9bxbmwi/image/upload/v1763557572/Dinh_Dang_Khoa_q33mxc.jpg' },
+
                     ],
+
                 ],
+
             },
+
         ],
+
     },
+
+
+
+
     {
         manager: {
             name: 'Ngo Anh Quan',
