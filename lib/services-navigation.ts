9769e41c5c9ebefb8460f0086693f4340b3c63f4
dinhdkhoa <kevin_mccain1718@yahoo.com>
2025-11-21 22:40:20 +0700

    fix: Order servicesNavigation

--- lib/services-navigation.ts
+++ lib/services-navigation.ts
@@ -1,37 +1,38 @@
 export const servicesNavigation = [
-    {
-        id: 1,
-        name: 'Planning',
-        label: 'Planning',
-        slug: 'planning',
-        pathname: '/our-services/planning',
-    },
-    {
-        id: 2,
-        name: 'Inbound Control',
-        label: 'Inbound Control',
-        slug: 'inbound-control',
-        pathname: '/our-services/inbound-control',
-    },
-    {
-        id: 3,
-        name: 'Master Data',
-        label: 'Master Data',
-        slug: 'master-data',
-        pathname: '/our-services/master-data',
-    },
-    {
-        id: 4,
-        name: 'Data Analytics',
-        label: 'Data Analytics',
-        slug: 'data-analytics',
-        pathname: '/our-services/data-analytics',
-    },
-    {
-        id: 5,
-        name: 'Logistics Projects',
-        label: 'Logistics Projects',
-        slug: 'logistics-projects',
-        pathname: '/our-services/logistics-projects',
-    },
-];
+  {
+    id: 3,
+    name: "Master Data",
+    label: "Master Data",
+    slug: "master-data",
+    pathname: "/our-services/master-data"
+  },
+  {
+    id: 1,
+    name: "Planning",
+    label: "Planning",
+    slug: "planning",
+    pathname: "/our-services/planning"
+  },
+  {
+    id: 2,
+    name: "Inbound Control",
+    label: "Inbound Control",
+    slug: "inbound-control",
+    pathname: "/our-services/inbound-control"
+  },
+
+  {
+    id: 4,
+    name: "Data Analytics",
+    label: "Data Analytics",
+    slug: "data-analytics",
+    pathname: "/our-services/data-analytics"
+  },
+  {
+    id: 5,
+    name: "Logistics Projects",
+    label: "Logistics Projects",
+    slug: "logistics-projects",
+    pathname: "/our-services/logistics-projects"
+  }
+]
