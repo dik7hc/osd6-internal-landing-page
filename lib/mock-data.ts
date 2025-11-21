45e493c686eb5874b80e88ead4127868ae2e9944
dinhdkhoa <kevin_mccain1718@yahoo.com>
2025-11-21 16:20:15 +0700

    content: Update Content

--- lib/mock-data.ts
+++ lib/mock-data.ts
@@ -36,12 +36,6 @@ const Activities: ActivityData[] = [
       "On September 4th – 5th, 2025, Philipp and Matthias have visited our team with a presentation that aimed to appreciate and ignite us with the mindset of building a #promisechain.In contrast to the old supply chain of storing goods and selling them to customer, this #promisechain is a circular loop that include planned promises being translated into full deliveries, going full circle back to the customer.Being a customer- forward model, many achievements have been made, across customer satisfaction, inventory, stock availability, and forecast accuracy.",
       "Looking into the future, Matthias has mentioned the upcoming pivot from internal improvements, to strengthening external relations.To understand more about the concept of #PromiseChain please click here."
     ]
-    // galleryImages: [
-    //     '/images/UXcellent1.jpeg',
-    //     '/images/UXcellent1.jpeg',
-    //     '/images/UXcellent1.jpeg',
-    //     '/images/UXcellent1.jpeg',
-    // ],
   },
   {
     slug: "Digi-awareness-Session-4",
@@ -49,19 +43,14 @@ const Activities: ActivityData[] = [
     date: "July 18th, 2025",
     author: "Pham Hong Ngoc ",
     mainImages: [
-      "https://res.cloudinary.com/dr9bxbmwi/image/upload/v1763569421/UXcellent1_lfup0h.jpg"
+      "https://res.cloudinary.com/dr9bxbmwi/image/upload/v1763716736/Digiawarness2_s0miw0.jpg",
+      "https://res.cloudinary.com/dr9bxbmwi/image/upload/v1763716734/Digiawareness_1_u2mwgd.jpg"
     ],
-    mainImageCaption: "UXcellent1",
+    mainImageCaption: "Session Images",
     content: [
       "On the July 18th, 2025, we had our 4th Digi-awareness session, learning about the different tips & tricks of SAP. Starting from the logon and home screen, we can add our favorite system / t-codes to top section, create folders to help us with navigation, change languages, and save these settings as a file for later uploads. Then, navigating from our home screen to each t-codes, some shortcuts can be used to open new ones / new windows, and close the current one even while running. Inside our t-codes, further personalization can be carried out with creating & saving variants, creating & saving layouts, and filtering & sorting tables before downloading as an excel file.",
       "As this session brings us a step closer to the last UXcellent session of the year, we hope that everyone will continue to keep up this professional positive energy, and finish the year strong with a new level in UX maturity."
     ]
-    // galleryImages: [
-    //     '/images/UXcellent1.jpeg',
-    //     '/images/UXcellent1.jpeg',
-    //     '/images/UXcellent1.jpeg',
-    //     '/images/UXcellent1.jpeg',
-    // ],
   },
   {
     slug: "Custom-Procedure-Workshop",
@@ -69,7 +58,7 @@ const Activities: ActivityData[] = [
     date: "July 18th, 2025",
     author: "Pham Hong Ngoc ",
     mainImages: [
-      "https://res.cloudinary.com/dr9bxbmwi/image/upload/v1763569421/UXcellent1_lfup0h.jpg"
+      "https://res.cloudinary.com/dr9bxbmwi/image/upload/v1763716678/customs_1_yfzl5q.jpg"
     ],
     mainImageCaption: "Workshop Images",
     content: [
@@ -77,12 +66,6 @@ const Activities: ActivityData[] = [
       "With the code now defined, declaring its customs value depends on our transaction values plus all payables before destination port, minus all costs beyond this point. However, as transaction values can be manipulated, 5 other methods are used in its stead, depending on certain situations.",
       "Finally, with our HS code and Customs value now ready to be written into the declaration form, we then go to the steps of calculating taxes, applying 4 main types: tariffs, VAT, SCT, and EPT. Fortunately, specifically for tariffs, this tax is reducible with MFN and FTA – through the utilization of C/O (country of origin). To decide whether our shipment is eligible, however, we must rely on these 4 rules: wholly obtained, change in tariff classification, regional value content, and specific process rule."
     ]
-    // galleryImages: [
-    //     '/images/UXcellent1.jpeg',
-    //     '/images/UXcellent1.jpeg',
-    //     '/images/UXcellent1.jpeg',
-    //     '/images/UXcellent1.jpeg',
-    // ],
   },
   {
     slug: "UXcellent-Session:-From-Pain-Points-to-Possibilities",
@@ -115,12 +98,6 @@ const Activities: ActivityData[] = [
       "On May 15th – 16th, our office has welcome 3 guests from GS/PSD6-DA: Kobold Miriam, Zhao Sylvia, and Xia Ningning. With an introductory session on their second day, the team got to know more about the full inbound cockpit project and the team members of the PSD6-DA team.",
       "With the cockpit now live, the chatbot is expected to be release soon with abilities in calculating numbers, and drawing quick charts – hence, answering our questions without having to export data onto excel. "
     ]
-    // galleryImages: [
-    //     '/images/UXcellent1.jpeg',
-    //     '/images/UXcellent1.jpeg',
-    //     '/images/UXcellent1.jpeg',
-    //     '/images/UXcellent1.jpeg',
-    // ],
   }
 ]
 
